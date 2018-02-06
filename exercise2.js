527d3b4e82a2f633fb6e8953bc11282cc73ca5db
JoviAndrew <jovian_andrew@hotmail.com>
2018-02-06 17:15:58 +0700

    rename exercise1 to exercise2 and exercise2 to exercise3

--- exercise2.js
+++ exercise2.js
@@ -1,18 +1,15 @@
-var input = [
-    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-]
+function balikString(kata){
+    let result = '';
 
-function dataHandling(arrayInput){
-    for(let index1 = 0; index1<arrayInput.length; index1++){
-        console.log('Nomor ID: '+arrayInput[index1][0]);
-        console.log('Nama Lengkap: '+arrayInput[index1][1]);
-        console.log('TTL: '+arrayInput[index1][2]+ ' '+ arrayInput[index1][3]);
-        console.log('Hobi: '+arrayInput[index1][4]);
-        console.log(''); //To give space after the first data
+    let counter = kata.length;
+    let counterResult = 0;
+    
+    while(counter > 0){
+     result += kata.substring(counter-1, counter);
+     counter--;
     }
+
+    return result;
 }
 
-dataHandling(input);
+console.log(balikString('Hello World!'));
--- exercise3.js
+++ exercise3.js
@@ -1,15 +1,18 @@
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
 
-function dataHandling2(arrayInput){
-    arrayInput.splice(1,1, arrayInput[1]+' Elsharawy');
-    arrayInput.splice(2, 1, 'Provinsi '+arrayInput[2]);
-    arrayInput.splice(4, 1, 'Pria', 'SMA International Metro');
-    return arrayInput;
+function dataHandling(arrayInput){
+    for(let index1 = 0; index1<arrayInput.length; index1++){
+        console.log('Nomor ID: '+arrayInput[index1][0]);
+        console.log('Nama Lengkap: '+arrayInput[index1][1]);
+        console.log('TTL: '+arrayInput[index1][2]+ ' '+ arrayInput[index1][3]);
+        console.log('Hobi: '+arrayInput[index1][4]);
+        console.log(''); //To give space after the first data
+    }
 }
 
-
-console.log(dataHandling2(input));
-
-/*Result: 
-["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"] 
-*/
+dataHandling(input);
